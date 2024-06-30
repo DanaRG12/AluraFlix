cb115aba4494ee27ebb73eb49d1eeed65245ba8e
Diana Robles <dana_rg@hotmail.com>
2024-06-29 22:52:24 -0600

    crear categoria titulo y agrupar por categoria las cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import api from './api';
-import Card from './components/Card';
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import api from "./api";
+import Card from "./components/Card";
 import GlobalStyles from "./components/GlobalStyles";
-import Home from './components/Pages/Home';
-import NuevoVideo from './components/Pages/NuevoVideo';
+import Home from "./components/Pages/Home";
+import NuevoVideo from "./components/Pages/NuevoVideo";
 import Cabecera from "./components/Cabecera";
-import Footer from './components/Footer';
-import Editar from './components/Modal/Editar';
+import Footer from "./components/Footer";
+import Editar from "./components/Modal/Editar";
+import FrontEnd from "./img/FrontEnd.png";
+import BackEnd from "./img/BackEnd.png";
+import Innova from "./img/Innova.png";
 
-
+//color el fondo degradado
 const FondoGradiente = styled.div`
   background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
 `;
 
-//abrir modal 
+//tamaño y fiorma del titulo de las cards
+const CategoriaTitulo = styled.img`
+  width: 400px;  
+  height: auto;
+  margin-bottom: 5px;
+`;
+
+//para abrir modal de editar
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [videos, setVideos] = useState([]);
@@ -49,6 +59,28 @@ function App() {
     setVideos(prevVideos => prevVideos.filter(video => video.id !== id));
     console.log('Eliminar video con id:', id);
   }
+//para el titulo de las categorias de las cards
+
+
+const agruparPorCategoria = (videos) => {
+  return videos.reduce((acc, video) => {
+    const { categoria } = video;
+    if (!acc[categoria]) acc[categoria] = [];
+    acc[categoria].push(video);
+    return acc;
+  }, {});
+};
+
+const categoriasAgrupadas = agruparPorCategoria(videos);
+
+const categoriaImagenes = {
+  Frontend: FrontEnd,
+  Backend: BackEnd,
+  'Innovación y Gestión': Innova,}
+
+
+
+  //estructura 
 
   return (
     <>
@@ -63,22 +95,32 @@ function App() {
         </Router>
 
 
-
-
-        {videos.map((video) => (
-          <Card
-            key={video.id}
-            id={video.id}
-            colorPrimario={video.categoria === 'Frontend' ? '#3498db' : video.categoria === 'Backend' ? '#2ecc71' : '#e74c3c'}
-            imagen={video.imagen}
-            titulo={video.titulo}
-            openModal={openModal}
-            eliminarVideo={eliminarVideo}
-          />
+        {Object.keys(categoriasAgrupadas).map((categoria) => (
+          <div key={categoria}>
+            <CategoriaTitulo src={categoriaImagenes[categoria]} alt={categoria} />
+            <div>
+              {categoriasAgrupadas[categoria].map((video) => (
+                <Card
+                  key={video.id}
+                  id={video.id}
+                  colorPrimario={
+                    video.categoria === 'Frontend' ? '#3498db' : 
+                    video.categoria === 'Backend' ? '#2ecc71' : 
+                    video.categoria === 'Innovación y Gestión' ? '#e74c3c' :
+                     '#000000'
+                    }
+                  imagen={video.imagen}
+                  titulo={video.titulo}
+                  openModal={openModal}
+                  eliminarVideo={eliminarVideo}
+                />
+              ))}
+            </div>
+          </div>
         ))}
-          
-              <Editar isModalOpen={isModalOpen} openModal={openModal} closeModal={closeModal} />
-      <Footer />
+
+        <Editar isModalOpen={isModalOpen} openModal={openModal} closeModal={closeModal} />
+        <Footer />
       </FondoGradiente>
     </>
   );
